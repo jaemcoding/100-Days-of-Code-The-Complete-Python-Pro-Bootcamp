e238abc6abb531160a8fb71fd495b77e2f214230
JAEMCODING <pjm910802@gmail.com>
2025-02-20 05:56:24 +0900

    Added Day 01

---
+++ Beginner/DAY 01 - Working with Variables in Python to Manage Data/1. Print.py
@@ -0,0 +1,7 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
---
+++ Beginner/DAY 01 - Working with Variables in Python to Manage Data/3. Input Function.py
@@ -0,0 +1,10 @@
+## 1)
+num1 = int(input())
+num2 = int(input())
+print(num1*num2)
+
+## 2)
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+print(len(input()))
